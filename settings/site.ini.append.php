f5d57f9de2697415df7df64b9a763ee57fdaf481
gggeek <gg@ez.no>
2010-05-08 14:42:27 +0000

    - info files
    git-svn-id: http://svn.projects.ez.no/ggsysinfo@40 3bcf0a6e-876b-4de1-acf0-8b12d1012fb4

--- settings/site.ini.append.php
+++ settings/site.ini.append.php
@@ -7,6 +7,11 @@ ExtensionAutoloadPath[]=ggsysinfo
 TranslationExtensions[]=ggsysinfo
 
 [RoleSettings]
+# There is no need to comment this out to insure proper security.
+# We use a slightly unusual permission model:
+# - all views check for access permissions by themselves
+# - we thus need to tell the kernel not to block access to the views
+# - using PolicyOmitList[]=sysinfo/oneview, it is possible to allow anon access to one specific view
 PolicyOmitList[]=sysinfo
 
 */ ?>
