e6782f182d1b3071fdab1702a6ea6a373c9f5b75
gggeek <gg@ez.no>
2010-05-09 16:30:20 +0000

    - storage churn graph: add a title with start and end date; use 10 points font; - storage churn graph: add a cache to avoid recalculating graph if storage log has not changed
    git-svn-id: http://svn.projects.ez.no/ggsysinfo@42 3bcf0a6e-876b-4de1-acf0-8b12d1012fb4

--- modules/sysinfo/storagechurn.php
+++ modules/sysinfo/storagechurn.php
@@ -7,12 +7,20 @@
  * @copyright (C) G. Giunta 2008-2010
  * @license Licensed under GNU General Public License v2.0. See file license.txt
  *
- * @todo derecred availability of gd2?
+ * @todo verify availability of gd2?
  * @todo add support for user-selected start and end date
  * @todo support coarser intervals than 60 secs
- * @todo add a caching layer or create the graph inline instead of stocking it on disk (works better in cluster mode)
  * @todo improve layout: graph padding, x axis labels, etc...
  */
+
+function calcChurnLabel( $pos, $step )
+{
+    $locale = eZLocale::instance();
+    /// todo: look at time span, if too big use date, if small use time (both will not fit, unless we change axis type)
+    // $out = $locale->formatShortDate( $pos );
+    $out = $locale->formatShortTime( $pos );
+    return $out;
+}
 
 $module = $Params['Module'];
 
@@ -28,97 +36,115 @@ if ( !in_array( 'sysinfo/storagechurn', $ini->variable( 'RoleSettings', 'PolicyO
     }
 }
 
-// *** Parse storage.log file ***
+// nb: this dir is calculated the same way as ezlog does
+$logfile = eZSys::varDirectory() . '/' . $ini->variable( 'FileSettings', 'LogDir' ) . '/storage.log';
+$cachedir = eZSys::cacheDirectory() . '/sysinfo';
+$cachefile = $cachedir . '/storagechurn.jpg';
 
-$scale = 60;
-$scalenames = array( 60 => 'minute', 60*60 => 'hour', 60*60*24 => 'day' );
-
-$ini = eZINI::instance();
-$file =  file( eZSys::varDirectory() . '/' . $ini->variable( 'FileSettings', 'LogDir' ) . '/storage.log' ); // nb: this dir is calculated the same way as ezlog does
-$data = array();
-foreach ( $file as $line )
+// *** Check if cached image file exists and is younger than storage log
+$cachefound = false;
+$clusterfile = eZClusterFileHandler::instance( $cachefile );
+if ( $clusterfile->exists() )
 {
-    $time = strtotime( substr( $line, 2, 20 ) );
-    $time = $time - ( $time % $scale );
-    if( !isset( $data[$time] ) )
+    $logdate = filemtime( $logfile );
+    $cachedate = $clusterfile->mtime();
+    if ( $cachedate >= $logdate )
     {
-        $data[$time] = 1;
+        $cachefound = true;
+        $clusterfile->fetch();
+    }
+}
+
+if ( !$cachefound )
+{
+    // *** Parse storage.log file ***
+
+    $scale = 60;
+    $scalenames = array( 60 => 'minute', 60*60 => 'hour', 60*60*24 => 'day' );
+
+    $ini = eZINI::instance();
+    $file =  file( $logfile );
+    $data = array();
+    foreach ( $file as $line )
+    {
+        $time = strtotime( substr( $line, 2, 20 ) );
+        $time = $time - ( $time % $scale );
+        if( !isset( $data[$time] ) )
+        {
+            $data[$time] = 1;
+        }
+        else
+        {
+            $data[$time]++;
+        }
+    }
+    $times = array_keys( $data );
+    $min = $times[0];
+    $max = end( $times );
+    /*
+       for ( $i = $min; $i <= $max; $i+= $scale )
+       {
+       $nodata[$i] = 0;
+       }
+       $data = array_merge( $nodata, $data );
+    */
+
+    // *** build graph ***
+
+    $graphname = ezi18n( 'SysInfo', 'Files per '.$scalenames[$scale] );
+
+    $graph = new ezcGraphBarChart();
+    $locale = eZLocale::instance();
+    $graph->title = "From " . $locale->formatShortDateTime( $min ) . " to " . $locale->formatShortDateTime( $max );
+    $graph->xAxis->label = "From " . $locale->formatShortDateTime( $min ) . " to " . $locale->formatShortDateTime( $max );
+    $graph->options->font->maxFontSize = 10;
+    $graph->palette = new ezcGraphPaletteEzBlue();
+    $graph->yAxis->label = $graphname;
+    $graph->legend = false;
+    // width of bar charts is not calculated correctly by DateAxis
+    //$graph->xAxis = new ezcGraphChartElementDateAxis();
+    //$graph->xAxis->interval = $scale;
+    $graph->xAxis = new ezcGraphChartElementNumericAxis();
+    $graph->xAxis->min = $min;
+    $graph->xAxis->max = $max;
+    $graph->xAxis->labelCallback = 'calcChurnLabel';
+    $graph->data[$graphname] = new ezcGraphArrayDataSet( $data );
+    $graph->driver = new ezcGraphGdDriver();
+    $graph->driver->options->imageFormat = IMG_JPEG;
+    // pick a font that is delivered along with ezp
+    $graph->options->font = 'design/standard/fonts/arial.ttf';
+
+    $outputdir = eZSys::rootDir() . '/' . $cachedir;
+    $outputfile = eZSys::rootDir() . '/' . $cachefile;
+    if ( !is_dir( $outputdir ) )
+    {
+        mkdir( $outputdir );
     }
     else
     {
-        $data[$time]++;
+        if ( is_file( $outputfile ) )
+        {
+            unlink( $outputfile );
+        }
     }
-}
-$times = array_keys( $data );
-$min = $times[0];
-$max = end( $times );
-/*
-   for ( $i = $min; $i <= $max; $i+= $scale )
-   {
-   $nodata[$i] = 0;
-   }
-   $data = array_merge( $nodata, $data );
-*/
-
-// *** build graph ***
-
-$graphname = ezi18n( 'SysInfo', 'Files per '.$scalenames[$scale] );
-
-$graph = new ezcGraphBarChart();
-$graph->title = ''; // title is in template
-$graph->palette = new ezcGraphPaletteEzBlue();
-$graph->yAxis->label = $graphname;
-$graph->legend = false;
-// width of bar charts is not calculated correctly by DateAxis
-//$graph->xAxis = new ezcGraphChartElementDateAxis();
-//$graph->xAxis->interval = $scale;
-$graph->xAxis = new ezcGraphChartElementNumericAxis();
-$graph->xAxis->min = $min;
-$graph->xAxis->max = $max;
-$graph->xAxis->labelCallback = 'calcChurnLabel';
-function calcChurnLabel( $pos, $step )
-{
-    $locale = eZLocale::instance();
-    /// todo: look at time span, if too big use date, if small use time (both will not fit, unless we change axis type)
-    // $out = $locale->formatShortDate( $pos );
-    $out = $locale->formatShortTime( $pos );
-    return $out;
-}
-$graph->data[$graphname] = new ezcGraphArrayDataSet( $data );
-$graph->driver = new ezcGraphGdDriver();
-$graph->driver->options->imageFormat = IMG_JPEG;
-// pick a font that is delivered along with ezp
-$graph->options->font = 'design/standard/fonts/arial.ttf';
-
-$outputdir = eZSys::rootDir() . '/' . eZSys::cacheDirectory() . '/sysinfo';
-if ( !is_dir( $outputdir ) )
-{
-    mkdir( $outputdir );
-}
-else
-{
-    if ( is_file( $outputdir . '/storagestats.jpg' ) )
+    try
     {
-        unlink( $outputdir . '/storagestats.jpg' );
+        $errormsg = "";
+        $graph->render( 600, 400, $outputfile );
+        $clusterfile->fileStore( $cachefile );
+    } catch( exception $e )
+    {
+        $errormsg = "Error while rendering graph: " . $e->getMessage();
+        eZDebug::writeError( $errormsg );
     }
 }
-try
-{
-    $errormsg = "";
-    $graph->render( 600, 400, $outputdir . '/storagestats.jpg' );
-} catch( exception $e )
-{
-    $errormsg = "Error while rendering graph: " . $e->getMessage();
-    eZDebug::writeError( $errormsg );
-}
-
 
 // *** output ***
 
 require_once( "kernel/common/template.php" );
 $tpl = templateInit();
 $tpl->setVariable( 'title', 'Storage churn' );
-$tpl->setVariable( 'graphsource', eZSys::cacheDirectory() . '/sysinfo/storagestats.jpg' );
+$tpl->setVariable( 'graphsource', $cachefile );
 $tpl->setVariable( 'errormsg', $errormsg );
 
 $Result = array();
