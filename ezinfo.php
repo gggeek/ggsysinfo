f2db0eb66b47bcf3791173d7d3d00d710a5e93a0
gggeek <gg@ez.no>
2010-10-25 00:19:16 +0000

    - update info file
    git-svn-id: http://svn.projects.ez.no/ggsysinfo@86 3bcf0a6e-876b-4de1-acf0-8b12d1012fb4

--- ezinfo.php
+++ ezinfo.php
@@ -3,9 +3,9 @@ class ggsysinfoInfo
 {
     static function info()
     {
-        return array( 'Name' => "ggsysinfo",
+        return array( 'Name' => "<a href=\"http://projects.ez.no/ggsysinfo\">ggsysinfo</a>",
                       'Version' => "0.3",
-                      'Copyright' => "Copyright (C) 2008-2010 G. Giunta",
+                      'Copyright' => "Copyright (C) 2008-2010 Gaetano Giunta",
                       'License' => "GNU General Public License v2.0"
                      );
     }
