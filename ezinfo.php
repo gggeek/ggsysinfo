2544e73118dbe0bb51eda35918e8373f1f96ee04
gggeek <gg@ez.no>
2010-11-01 18:09:25 +0000

    - tag for next release
    git-svn-id: http://svn.projects.ez.no/ggsysinfo@106 3bcf0a6e-876b-4de1-acf0-8b12d1012fb4

--- ezinfo.php
+++ ezinfo.php
@@ -4,7 +4,7 @@ class ggsysinfoInfo
     static function info()
     {
         return array( 'Name' => "<a href=\"http://projects.ez.no/ggsysinfo\">ggsysinfo</a>",
-                      'Version' => "0.4",
+                      'Version' => "0.5-dev",
                       'Copyright' => "Copyright (C) 2008-2010 Gaetano Giunta",
                       'License' => "GNU General Public License v2.0"
                      );
