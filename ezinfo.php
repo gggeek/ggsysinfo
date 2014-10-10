b280aebac3296e181cbd503faeb782136d905490
gggeek <gg@ez.no>
2014-10-10 17:30:01 +0200

    Tag for 0.8 development sprint

--- ezinfo.php
+++ ezinfo.php
@@ -4,7 +4,7 @@ class ggsysinfoInfo
     static function info()
     {
         return array( 'Name' => "<a href=\"http://projects.ez.no/ggsysinfo\">ggsysinfo</a>",
-                      'Version' => "0.7.0",
+                      'Version' => "0.8-dev",
                       'Copyright' => "Copyright (C) 2008-2014 Gaetano Giunta",
                       'License' => "GNU General Public License v2.0"
                      );
