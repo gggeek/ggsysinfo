20d95fb24dd8dae11da4eacdc1798f9816ec3fce
gggeek <gg@ez.no>
2014-10-10 20:57:39 +0200

    Remove leftover Ovum stuff; add more info in realpath-cache view

--- modules/sysinfo/realpath.php
+++ modules/sysinfo/realpath.php
@@ -4,7 +4,8 @@ $tpl->setVariable(
     'realpath_cache',
     array(
         'available' => ini_get( 'realpath_cache_size' ),
-        'ttl' => ini_get( 'realpath_cache_ttl' ),
-        'used' => realpath_cache_size()
+        'used' => realpath_cache_size(),
+        'entries' => count( realpath_cache_get() ),
+        'ttl' => ini_get( 'realpath_cache_ttl' )
     )
 );
