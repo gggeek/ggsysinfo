dc716f9e0de0d2b18cdfce209dbab6adbdd1bf6e
gggeek <gg@ez.no>
2014-10-13 08:55:49 +0200

    Update documentation

--- ezinfo.php
+++ ezinfo.php
@@ -7,7 +7,7 @@ class ggsysinfoInfo
                       'Version' => "0.8-dev",
                       'Copyright' => "Copyright (C) 2008-2014 Gaetano Giunta",
                       'License' => "GNU General Public License v2.0"
-                     );
+                    );
     }
 }
 ?>
