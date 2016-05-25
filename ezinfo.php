e47b89a4592bec2ce0baf257160cfa8f05e0ca71
gggeek <gg@ez.no>
2016-05-25 15:27:54 +0100

    Add changelogs and tag 0.9.3

--- ezinfo.php
+++ ezinfo.php
@@ -1,13 +1,13 @@
 <?php
+
 class ggsysinfoInfo
 {
     static function info()
     {
         return array( 'Name' => "<a href=\"http://projects.ez.no/ggsysinfo\">ggsysinfo</a>",
-                      'Version' => "0.9.2",
+                      'Version' => "0.9.3",
                       'Copyright' => "Copyright (C) 2008-2016 Gaetano Giunta",
                       'License' => "GNU General Public License v2.0"
                     );
     }
 }
-?>
